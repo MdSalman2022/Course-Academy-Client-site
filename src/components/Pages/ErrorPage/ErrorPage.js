55d21d15b381275a126c3f16a766331962f64d38
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-10-27 15:59:02 +0600

    error page added

--- src/components/Pages/ErrorPage/ErrorPage.js
+++ src/components/Pages/ErrorPage/ErrorPage.js
@@ -6,10 +6,10 @@ export default function ErrorPage() {
 
     return (
         <div id="error-page">
-            <h1 className="text-9xl font-bold text-warning my-20">404</h1>
+            <h1 className="text-9xl font-bold text-primary my-20">404</h1>
             <h1 className="text-4xl my-10 font-bold">OPPS!PAGE NOT FOUND</h1>
             <p className="text-2xl my-10">Sorry, the page you're looking for doesn't exist</p>
-            <Link to='/'> <button className="btn btn-block container mx-auto my-20">Return Home</button></Link>
+            <Link to='/'> <button className="btn btn-block btn-primary container mx-auto my-20">Return Home</button></Link>
         </div>
     );
 }
